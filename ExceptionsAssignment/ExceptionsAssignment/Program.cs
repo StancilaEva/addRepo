e56414a5df5e68372909c3ad4d5a2ca25e95b875
Eva Stancila <eva.stancila@amdaris.com>
2022-03-17 11:23:40 +0200

    added exceptions assignment

---
+++ ExceptionsAssignment/ExceptionsAssignment/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using ExceptionsAssignment;
+
+
+
+
+bool isValid(string s)
+{
+    if (s == null)
+    {
+        throw new StringIsNullException("string is null");
+    }
+    else
+        if(s.Length == 0)
+    {
+        throw new StringIsEmptyException("string is empty");
+    }
+    return s.Length > 6;
+}
+
+try
+{
+    try
+    {
+        if (isValid(null))
+        {
+            Console.WriteLine("is Valid");
+        }
+    }
+    catch (StringIsNullException ex)
+    {
+        Console.WriteLine(ex.Message);
+        throw new Exception("null string exception", ex);
+    }
+    catch (StringIsEmptyException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    finally
+    {
+        Console.WriteLine("execution of finally");
+    }
+}catch(Exception ex)
+{
+    Console.WriteLine(ex.Message+" exception message: "+ex.InnerException.Message);
+}
+
+#if DEBUG
+Console.WriteLine("debug");
+#endif
---
+++ ExceptionsAssignment/ExceptionsAssignment/StringIsEmptyException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsAssignment
+{
+    internal class StringIsEmptyException : Exception
+    {
+        public StringIsEmptyException()
+        {
+        }
+
+        public StringIsEmptyException(string? message) : base(message)
+        {
+        }
+    }
+}
